2e17fa8e30d86086ab7f33ebb206c1d62bae75b7
Елизавета Фитиль <nikameredit26@gmail.com>
2025-06-18 17:38:37 +0300

    Update warehouse.sql

--- src/warehouse.sql
+++ src/warehouse.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1
--- Время создания: Июн 18 2025 г., 16:35
--- Версия сервера: 10.4.28-MariaDB
--- Версия PHP: 8.2.4
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
